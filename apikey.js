b187bcbda085d7694c4a53add56405984726933d
D4nzyXcripter <ampremcuki123@gmail.com>
2026-08-06 11:16:16 +0700

    Update apikey.js

--- apikey.js
+++ apikey.js
@@ -1,5 +1,5 @@
 [
   {"key": "puqi", "ordername": "DHANNONLY-6767", "codeorder": true, "codesuffix": false},
   {"key": "free-api2026", "ordername": "VOo", "codeorder": true, "codesuffix": "FREE"},
-  {"key": "freeapikeydhan26", "ordername": "DHANNONLY-67", "codeorder": false, "codesuffix": "GUEST"}
+  {"key": "freeapikeydhan26", "ordername": "DHANNONLY-67", "codeorder": false, "codesuffix": false}
 ]
