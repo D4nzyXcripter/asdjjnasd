b5f6a39c8d6acda8e131fff1631928276591868c
D4nzyXcripter <ampremcuki123@gmail.com>
2026-08-09 14:59:47 +0700

    Update apikey.js

--- apikey.js
+++ apikey.js
@@ -4,5 +4,6 @@
   {"key": "freeapikeydhan26", "ordername": "DHANNONLY-67", "codeorder": false, "codesuffix": false},
   {"key": "Kyuu", "ordername": "KyuuXzry", "codeorder": true, "codesuffix": false},
   {"key": "LUCIFER", "ordername": "LUCIFER", "codeorder": true, "codesuffix": false},
-  {"key": "maskayapikey", "ordername": "Maskay456", "codeorder": true, "codesuffix": false}
+  {"key": "maskayapikey", "ordername": "Maskay456", "codeorder": true, "codesuffix": false},
+  {"key": "Dunz", "ordername": "𝙳𝚄𝙽𝚉𝙿𝚁𝙸𝙼𝙴", "codeorder": true, "codesuffix": false},
 ]
