a5e1229c522f028891be2c3e3c309f5086cb0ede
D4nzyXcripter <ampremcuki123@gmail.com>
2026-08-09 17:44:40 +0700

    update apikey.js

--- apikey.js
+++ apikey.js
@@ -1,8 +1,44 @@
 [
-  {"key": "puqi", "ordername": "DHANNONLY-6767", "codeorder": true, "codesuffix": false},
-  {"key": "free-api2026", "ordername": "VOo", "codeorder": true, "codesuffix": "FREE"},
-  {"key": "freeapikeydhan26", "ordername": "DHANNONLY-67", "codeorder": false, "codesuffix": false},
-  {"key": "Kyuu", "ordername": "KyuuXzry", "codeorder": true, "codesuffix": false},
-  {"key": "LUCIFER", "ordername": "LUCIFER", "codeorder": true, "codesuffix": false},
-  {"key": "maskayapikey", "ordername": "Maskay456", "codeorder": true, "codesuffix": false}
+  {
+    "key": "puqi",
+    "ordername": "DHANNONLY-6767",
+    "codeorder": true,
+    "codesuffix": false
+  },
+  {
+    "key": "free-api2026",
+    "ordername": "VOo",
+    "codeorder": true,
+    "codesuffix": "FREE"
+  },
+  {
+    "key": "freeapikeydhan26",
+    "ordername": "DHANNONLY-67",
+    "codeorder": false,
+    "codesuffix": false
+  },
+  {
+    "key": "Kyuu",
+    "ordername": "KyuuXzry",
+    "codeorder": true,
+    "codesuffix": false
+  },
+  {
+    "key": "LUCIFER",
+    "ordername": "LUCIFER",
+    "codeorder": true,
+    "codesuffix": false
+  },
+  {
+    "key": "maskayapikey",
+    "ordername": "Maskay456",
+    "codeorder": true,
+    "codesuffix": false
+  },
+  {
+    "key": "tess",
+    "ordername": "teslah",
+    "codeorder": true,
+    "codesuffix": false
+  }
 ]
