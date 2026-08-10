3afe79b280f8580100602fd92819994cc49654b7
D4nzyXcripter <ampremcuki123@gmail.com>
2026-08-10 15:59:28 +0700

    update apikey.js

--- apikey.js
+++ apikey.js
@@ -40,5 +40,11 @@
     "ordername": "Imxxzz",
     "codeorder": true,
     "codesuffix": false
+  },
+  {
+    "key": "Dunz",
+    "ordername": "𝙳𝚄𝙽𝚉𝙿𝚁𝙸𝙼𝙴",
+    "codeorder": true,
+    "codesuffix": false
   }
 ]
