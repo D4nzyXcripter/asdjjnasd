441eef9039b4e94611a5b8c09d89e9166861242d
D4nzyXcripter <ampremcuki123@gmail.com>
2026-09-18 17:08:32 +0700

    update apikey.js [2026-09-18T10:08:31.859Z]

--- apikey.js
+++ apikey.js
@@ -1,65 +1,68 @@
-[
-  {
-    "key": "freeapikeydhan26",
-    "ordername": "BETTADHANN",
-    "codeorder": false,
-    "codesuffix": false,
-    "id": 1
-  },
-  {
-    "key": "Kyuu",
-    "ordername": "KyuuXzry",
-    "codeorder": true,
-    "codesuffix": false,
-    "id": 2
-  },
-  {
-    "key": "LUCIFER",
-    "ordername": "LUCIFER",
-    "codeorder": true,
-    "codesuffix": false,
-    "id": 3
-  },
-  {
-    "key": "maskayapikey",
-    "ordername": "Maskay456",
-    "codeorder": true,
-    "codesuffix": false,
-    "id": 4
-  },
-  {
-    "key": "astabot",
-    "ordername": "Imxxzz",
-    "codeorder": true,
-    "codesuffix": false,
-    "id": 5
-  },
-  {
-    "key": "Dunz",
-    "ordername": "𝙳𝚄𝙽𝚉𝙿𝚁𝙸𝙼𝙴",
-    "codeorder": true,
-    "codesuffix": false,
-    "id": 6
-  },
-  {
-    "key": "dravndesamuel",
-    "ordername": "DRAVN1303",
-    "codeorder": true,
-    "codesuffix": false,
-    "id": 7
-  },
-  {
-    "key": "chris",
-    "ordername": "Sir-Chris",
-    "codeorder": true,
-    "codesuffix": false,
-    "id": 8
-  },
-  {
-    "key": "sirdhannapi",
-    "ordername": "ꦽꦽsirdhannꦽꦽ",
-    "codeorder": true,
-    "codesuffix": false,
-    "id": 9
-  }
-]
+{
+  "keys": [
+    {
+      "key": "freeapikeydhan26",
+      "ordername": "BETTADHANN",
+      "codeorder": false,
+      "codesuffix": false,
+      "id": 1
+    },
+    {
+      "key": "Kyuu",
+      "ordername": "KyuuXzry",
+      "codeorder": true,
+      "codesuffix": false,
+      "id": 2
+    },
+    {
+      "key": "LUCIFER",
+      "ordername": "LUCIFER",
+      "codeorder": true,
+      "codesuffix": false,
+      "id": 3
+    },
+    {
+      "key": "maskayapikey",
+      "ordername": "Maskay456",
+      "codeorder": true,
+      "codesuffix": false,
+      "id": 4
+    },
+    {
+      "key": "astabot",
+      "ordername": "Imxxzz",
+      "codeorder": true,
+      "codesuffix": false,
+      "id": 5
+    },
+    {
+      "key": "Dunz",
+      "ordername": "𝙳𝚄𝙽𝚉𝙿𝚁𝙸𝙼𝙴",
+      "codeorder": true,
+      "codesuffix": false,
+      "id": 6
+    },
+    {
+      "key": "dravndesamuel",
+      "ordername": "DRAVN1303",
+      "codeorder": true,
+      "codesuffix": false,
+      "id": 7
+    },
+    {
+      "key": "chris",
+      "ordername": "Sir-Chris",
+      "codeorder": true,
+      "codesuffix": false,
+      "id": 8
+    },
+    {
+      "key": "sirdhannapi",
+      "ordername": "ꦽꦽsirdhannꦽꦽ",
+      "codeorder": true,
+      "codesuffix": false,
+      "id": 9
+    }
+  ],
+  "maintenance": true
+}
